54b497aa4cd61be64be1cf5f9287697bbccdf0cd
skagur-k <skagur0329@gmail.com>
2022-05-22 00:39:19 +0900

    initial ui

---
+++ components/Sidebar.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import {
+  BellIcon,
+  HashtagIcon,
+  BookmarkIcon,
+  CollectionIcon,
+  DotsCircleHorizontalIcon,
+  MailIcon,
+  UserIcon,
+  HomeIcon,
+} from '@heroicons/react/outline'
+import Image from 'next/image'
+import TwitterLogo from '../public/twitter.png'
+import SidebarRow from './SidebarRow'
+
+function Sidebar() {
+  return (
+    <div className="col-span-2 flex flex-col items-center px-4 md:items-start">
+      <div className="relative m-3 h-10 w-10">
+        <Image
+          src={TwitterLogo}
+          alt="Twitter Icon"
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
+      <SidebarRow Icon={HomeIcon} title="Home" />
+      <SidebarRow Icon={HashtagIcon} title="Explore" />
+      <SidebarRow Icon={BellIcon} title="Notifications" />
+      <SidebarRow Icon={MailIcon} title="Messages" />
+      <SidebarRow Icon={BookmarkIcon} title="Bookmarks" />
+      <SidebarRow Icon={CollectionIcon} title="Lists" />
+      <SidebarRow Icon={UserIcon} title="Sign In" />
+      <SidebarRow Icon={DotsCircleHorizontalIcon} title="More" />
+    </div>
+  )
+}
+
+export default Sidebar
---
+++ components/Widgets.tsx
@@ -0,0 +1,26 @@
+import { SearchIcon } from '@heroicons/react/outline'
+import React from 'react'
+import { TwitterTimelineEmbed } from 'react-twitter-embed'
+function Widgets() {
+  return (
+    <div className="col-span-2 mt-2 hidden px-2 lg:inline">
+      {/* Search */}
+      <div className="mt-2 flex items-center space-x-2 rounded-full bg-gray-100 p-3">
+        <SearchIcon className="h-5 w-5 text-gray-400" />
+        <input
+          className="flex-1 bg-transparent outline-none"
+          type="text"
+          placeholder="Search Tweet"
+        />
+      </div>
+      {/* Twitter Timeline embed */}
+      <TwitterTimelineEmbed
+        sourceType="profile"
+        screenName="FabrizioRomano"
+        options={{ height: 1000 }}
+      />
+    </div>
+  )
+}
+
+export default Widgets
